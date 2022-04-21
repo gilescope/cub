cc0062cd12c1fc2bc1f31221d06fb70ab5ca34bb
Giles Cope <gilescope@gmail.com>
2022-04-21 10:02:11 +0100

    --dev is used by default unless you add in a basepath.

---
+++ fetch_weights.sh
@@ -0,0 +1,4 @@
+scp -r bm2:cumulus/polkadot-parachains/statemine/src/weights/ ./cumulus/polkadot-parachains/statemine/src/
+scp -r bm2:cumulus/polkadot-parachains/statemint/src/weights/ ./cumulus/polkadot-parachains/statemint/src/
+scp -r bm2:cumulus/polkadot-parachains/westmint/src/weights/ ./cumulus/polkadot-parachains/westmint/src/
+
